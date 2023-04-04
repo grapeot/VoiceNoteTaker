a6b84fbdb0c9e5dba57413659b780b578d5d66c4
Yan Wang <grapeot@outlook.com>
2023-04-04 17:38:18 +0000

    Add serialization and a few new commands around privacy in the telegram bot.

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,8 @@
 import os
 import tempfile
+import json
 from telegram import Update
-from telegram.ext import CommandHandler, MessageHandler, CallbackContext, Application
+from telegram.ext import CommandHandler, MessageHandler, CallbackContext, Application, PicklePersistence
 import telegram.ext.filters as filters
 
 import core
@@ -10,12 +11,68 @@ from core import paraphrase_text, convert_audio_file_to_format
 OUTPUT_FORMAT = "mp3"
 
 telegram_api_token = os.environ.get('TELEGRAM_BOT_TOKEN')
-print(telegram_api_token)
+print(f'Bot token: {telegram_api_token}')
 
 async def start(update: Update, context: CallbackContext):
     await update.message.reply_text('Send me a voice message, and I will transcribe it for you. Note I am not a QA bot, and will not answer your questions. I will only listen to you and transcribe your voice message, with paraphrasing from GPT-4.')
 
+async def help(update: Update, context: CallbackContext):
+    await update.message.reply_text("""*YaGe Voice Note Taker Bot*
+
+*Usage*: Send me a voice message, and I will transcribe it for you\. Note I am not a QA bot, and will not answer your questions\. I will only listen to you and transcribe your voice message, with paraphrasing from GPT\-4\.
+
+*Data and privacy*: I log your transcriptions and paraphrased texts, to support a future service of sending summary of your voice messages\. I will not share your data with any third party\. I will not use your data for any purposes other than to provide you with a better service\. You can always check what data are logged by sending /data command, and clear your data \(on our end\) by sending /clear command\.
+
+*Commands*: 
+/help: Display this help message\.
+/data: Display any information we had about you from our end\.
+/clear: Clear any information we had about you from our end\.""", parse_mode='MarkdownV2')
+
+async def data(update: Update, context: CallbackContext):
+    """
+    Display any information we had about the user from our end.
+    """
+    chat_id = context._chat_id
+    user_id = context._user_id
+    member = await context.bot.get_chat_member(chat_id, user_id)
+    user_full_name = member.user.full_name
+    print(f'[{user_full_name}] /data')
+    await update.message.reply_text(str(context.user_data))
+
+async def clear(update: Update, context: CallbackContext):
+    """
+    Clear any information we had about the user from our end.
+    """
+    chat_id = context._chat_id
+    user_id = context._user_id
+    member = await context.bot.get_chat_member(chat_id, user_id)
+    user_full_name = member.user.full_name
+    print(f'[{user_full_name}] /clear')
+    context.user_data.clear()
+    await update.message.reply_text("Your data has been cleared.")
+
+# TODO: send out daily summaries to users.
+
+async def warn_if_not_voice_message(update: Update, context: CallbackContext):
+    if not update.message.voice:
+        await update.message.reply_text("Please send me a voice message. I will transcribe it and paraphrase for you.")
+
 async def transcribe_voice_message(update: Update, context: CallbackContext):
+    chat_id = context._chat_id
+    user_id = context._user_id
+    member = await context.bot.get_chat_member(chat_id, user_id)
+    user_full_name = member.user.full_name
+    # We need to log the user info and histories in the user_data so we can send out daily summaries.
+    # Check the help message for more details.
+    if 'user_full_name' not in context.user_data:
+        context.user_data['user_full_name'] = user_full_name
+    if 'user_id' not in context.user_data:
+        context.user_data['user_id'] = user_id
+    if 'transcript' not in context.user_data:
+        context.user_data['transcript'] = []
+    if 'paraphrased' not in context.user_data:
+        context.user_data['paraphrased'] = []
+    
     file_id = update.message.voice.file_id
     voice_file = await context.bot.get_file(file_id)
     
@@ -29,25 +86,37 @@ async def transcribe_voice_message(update: Update, context: CallbackContext):
         with tempfile.NamedTemporaryFile(suffix=f'.{OUTPUT_FORMAT}') as temp_output_file:
             convert_audio_file_to_format(temp_audio_file.name, temp_output_file.name, OUTPUT_FORMAT)
             transcribed_text = core.transcribe_voice_message(temp_output_file.name)
-    print(transcribed_text)
+    print(f'[{user_full_name}] {transcribed_text}')
+    context.user_data['transcript'].append({
+        'date': update.message.date,
+        'content': transcribed_text,
+
+    })
     await update.message.reply_text("Transcribed text:")
     await update.message.reply_text(transcribed_text)
 
-    # TODO: change to GPT-4 for whitelisted users
     paraphrased_text = paraphrase_text(transcribed_text, 'gpt-4')
-    print(paraphrased_text)
+    print(f'[{user_full_name}] {paraphrased_text}')
+    context.user_data['paraphrased'].append({
+        'date': update.message.date,
+        'content': paraphrased_text,
+    })
     await update.message.reply_text("Paraphrased using GPT-4:")
     await update.message.reply_text(paraphrased_text)
 
 def main():
-    application = Application.builder().token(telegram_api_token).build()
+    persistence = PicklePersistence(filepath="gpt_archive.pickle")
+    application = Application.builder().token(telegram_api_token).persistence(persistence).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", start))
+    application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler("clear", clear))
+    application.add_handler(CommandHandler("data", data))
 
-    # on non command i.e message - echo the message on Telegram
+    # on non command i.e message
     application.add_handler(MessageHandler(filters.VOICE & ~filters.COMMAND, transcribe_voice_message))
+    application.add_handler(MessageHandler(~filters.VOICE & ~filters.COMMAND, warn_if_not_voice_message))
 
     # Run the bot until the user presses Ctrl-C
     print('Bot is running...')
